ed69015f99e2bb9bdcaf13527d71c4a7a5b0584f
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-21 20:17:34 +0900

    New | Sqlite
    Sqlite Class를 이용한 생성자 함수 생성
    생성자 함수로 해당 경로의 DB의 접속을 시도한다
    경로에 DB가 존재하지 않다면 만들고 접속한다

---
+++ module/Databases/Sqlite.js
@@ -0,0 +1,10 @@
+const Database = require("better-sqlite3");
+const DbPathCheck = require("./DbPathCheck");
+
+//TODO : Sqlite Query문 실행에 관한 메소드가 필요합니다.
+class Sqlite {
+  constructor(dbName){
+    //* dbName으로 Database 객체를 생성한다.
+    this.db =  new Database(DbPathCheck(dbName), {verbose : console.log});
+  }
+}
