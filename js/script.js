56794d25579301379ebec5b5d5089e9d3a7a7bea
Saccharin01 <chousik01@gmail.com>
2024-06-25 17:19:19 +0900

    comment | reDirect.js
    추후에 필요한 부분 코맨트 및 setTimeout의 ms값을 매개변수로 받는 조정

--- js/script.js
+++ js/script.js
@@ -7,26 +7,22 @@ const testBed = (timeOut)=>{
 
 
 
-function reDirect(something){
-document.addEventListener("DOMContentLoaded", ()=>{
-  setTimeout(()=>{
-    window.location.href = `${something}`
-  })
+function reDirect(location, loadTime){ // ? location = 이동을 원하는 url / loadTime = 몇 초 안에?
 
-})
+  document.addEventListener("DOMContentLoaded", ()=>{ // ? 페이지가 다 로드 되었을 때 시작될 이벤트리스너
+
+    setTimeout(()=>{
+      window.location.href = `${location}` // ? something 매개변수는 리다이렉트를 걸 html 링크로 할 예정
+    },loadTime)
+  })
 }
 
+// todo 위의 함수를 생성자 함수 + 매개변수의 데이터타입 검역 절차를 추가해보는 건 어떨까?
 /**
+ * todo 단위테스트 : 돔 컨텐츠가 로드 되기 전에는 어떻게 되는지?
+ * todo : 움직이는 부분을 UX를 생각해서 인디케이터 같은 걸 넣어야 하지 않나? (우리가 만든다고 했던 로딩 바 같은거)
+ * todo 단위테스트 :
+ * todo 단위테스트 :
+ * todo 단위테스트 :
  * 
- * let seconds = 5;
-    function countdown() {
-      document.getElementById('countdown').textContent = seconds;
-      if (seconds > 0) {
-        seconds--;
-        setTimeout(countdown, 1000);
-      } else {
-        window.location.href = 'b.html';
-      }
-    }
-    window.onload = countdown;
  */
