44855ee96797c206fd3d5347a8d7b60e32afd846
Saccharin01 <chousik01@gmail.com>
2024-06-27 14:23:07 +0900

    new | mimeTypes.js / readFile.js
    정적 파일을 서빙하는 코드를 모듈로 분리합니다.

---
+++ module/mimeTypes.js
@@ -0,0 +1,9 @@
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'application/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+};
