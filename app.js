313150bf9e315113c3dd1cb79fa1a905825aabb4
Saccharin01 <chousik01@gmail.com>
2024-06-27 11:47:41 +0900

    fix | app.js
    pull Request 병합 충돌이 해결됨에 따라 rebase를 이용해서 분기를 재정립.
    정적 파일 내의 css 파일 calling 해결 할 예정

--- app.js
+++ app.js
@@ -39,15 +39,16 @@ let server = http.createServer((req, res)=>{
       })
     }
     else if(req.url.split(".")[1] === "js"){
-      console.log(__dirname)
+      // console.log(__dirname)
       let jspath = path.join(__dirname,"js",req.url.split("/")[1])
-      console.log(jspath)
+      // console.log(jspath)
       fs.readFile(jspath,"utf-8",(err,data)=>{
         if(err){
           res.writeHead(500, {"content-type" : "text/plain"})
           res.write("server error")
         } else {
           // console.log(data)
+          // todo 컨텐츠 타입
           res.writeHead(200, {"content-type" : "text/html"})
           res.write(data)
           res.end()
